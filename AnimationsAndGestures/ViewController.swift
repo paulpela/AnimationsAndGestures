8812df3fe827983477ce03a50d38e63955db371c
Paweł Pela <paulpela@gmail.com>
2018-12-14 12:50:59 +0100

    Initial Commit

---
+++ AnimationsAndGestures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationsAndGestures
+//
+//  Created by Paweł Pela on 14/12/2018.
+//  Copyright © 2018 Paweł Pela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
